9331d5ec98a9c58998c59577c35c813655f7806b
Gustavo Souza <gustavo_souza.1996@hotmail.com>
2018-05-17 02:41:08 -0300

    att dia 17/05/2018

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,2 +1,2 @@
 //Constante para carregar o arquivo json
-export const DAYCARE = 'http://localhost:3000'
+export const DAYCARE = 'https://appdbteste2.herokuapp.com'
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import {ROUTES} from './app.routes';
+import { ROUTES } from './app.routes';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -14,13 +14,16 @@ import { SobreComponent } from './sobre/sobre.component'
 import { ImportResolver } from '@angular/compiler';
 import { CrechesComponent } from './creches/creches.component';
 import { CrecheComponent } from './creches/creche/creche.component';
-import {CrechesService} from './creches/creches.service';
+import { CrechesService } from './creches/creches.service';
 import { CrecheDetalhesComponent } from './creche-detalhes/creche-detalhes.component';
 import { LocalizacaoComponent } from './creche-detalhes/localizacao/localizacao.component';
 import { ServicoItemComponent } from './creche-detalhes/servico-item/servico-item.component';
 import { ServicoComponent } from './creche-detalhes/servico/servico.component';
 import { AvaliacoesComponent } from './creche-detalhes/avaliacoes/avaliacoes.component';
 import { DivulgarComponent } from './divulgar/divulgar.component';
+
+//módulo do mapa
+import { AgmCoreModule } from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -38,12 +41,18 @@ import { DivulgarComponent } from './divulgar/divulgar.component';
     DivulgarComponent
   ],
   imports: [
+
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
     HttpModule,
-    RouterModule.forRoot(ROUTES)
+    RouterModule.forRoot(ROUTES),
+    AgmCoreModule.forRoot({
+      apiKey: "AIzaSyB25O-GoozqDll1vzOPTh71JpqeX_VJfCo",
+      libraries: ["places"]
+    }),
+
   ],
   providers: [CrechesService],
   bootstrap: [AppComponent]
